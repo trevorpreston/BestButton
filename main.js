5484dd4f1346c3eb1bd8d19672b3a0a9c4106a2e
Trevor Preston <trevorpreston4@gmail.com>
2016-09-20 11:02:12 -0400

    added the purple box

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('javascript is connected!')
+
+var $body = document.body
+var button = document.getElementById('sup')
+
+function sup(){
+  alert("sup")
+}
+
+button.addEventListener('click', sup)
