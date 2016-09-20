339313864682eec4b5b4281eba6ce791c0c59865
Trevor Preston <trevorpreston4@gmail.com>
2016-09-20 11:17:32 -0400

    it's actually a button

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 console.log('javascript is connected!')
 
-var $body = document.body
-var button = document.getElementById('sup')
+var $body = document.body;
+var button = document.getElementById('sup');
 
 function sup(){
-  alert("sup")
+  alert('sup');
 }
 
 button.addEventListener('click', sup)
